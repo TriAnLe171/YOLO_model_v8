ac55a86288e24c96ae11078001947d6b070119ff
tdle26 <ldta1712004@gmail.com>
2024-10-18 16:23:04 -0400

    first commit

---
+++ web_object_detector.py
@@ -0,0 +1,73 @@
+from ultralytics import YOLO
+from flask import request, Response, Flask
+from waitress import serve
+from PIL import Image
+import json
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def root():
+    """
+    Site main page handler function.
+    :return: Content of index.html file
+    """
+    index_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates/index.html")
+    with open(index_path) as file:
+        return file.read()
+
+
+@app.route("/detect", methods=["POST"])
+def detect():
+    """
+    Handler of /detect POST endpoint
+    Receives uploaded file with a name "image_file",
+    passes it through YOLOv8 object detection
+    network and returns an array of bounding boxes.
+    :return: a JSON array of objects bounding
+    boxes in format
+    [[x1,y1,x2,y2,object_type,probability],..]
+    """
+    buf = request.files["image_file"]
+    boxes = detect_objects_on_image(Image.open(buf.stream))
+    return Response(json.dumps(boxes), mimetype="application/json")
+
+
+def detect_objects_on_image(buf):
+    """
+    Function receives an image,
+    passes it through YOLOv8 neural network
+    and returns an array of detected objects
+    and their bounding boxes
+    :param buf: Input image file stream
+    :return: Array of bounding boxes in format
+    [[x1,y1,x2,y2,object_type,probability],..]
+    """
+    model = YOLO("yolov8x.pt")
+    results = model.predict(buf)
+    result = results[0]
+    output = []
+    for box in result.boxes:
+        x1, y1, x2, y2 = [round(x) for x in box.xyxy[0].tolist()]
+        class_id = box.cls[0].item()
+        prob = round(box.conf[0].item(), 2)
+        output.append([x1, y1, x2, y2, result.names[class_id], prob])
+    return output
+
+
+app.debug = True
+
+serve(app, host="127.0.0.1", port=8081) #http://127.0.0.1:8081
+
+# {0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle', 4: 'airplane', 5: 'bus', 6: 'train', 7: 'truck', 8: 'boat', 
+# 9: 'traffic light', 10: 'fire hydrant', 11: 'stop sign', 12: 'parking meter', 13: 'bench', 14: 'bird', 15: 'cat', 
+# 16: 'dog', 17: 'horse', 18: 'sheep', 19: 'cow', 20: 'elephant', 21: 'bear', 22: 'zebra', 23: 'giraffe', 24: 'backpack', 
+# 25: 'umbrella', 26: 'handbag', 27: 'tie', 28: 'suitcase', 29: 'frisbee', 30: 'skis', 31: 'snowboard', 32: 'sports ball', 
+# 33: 'kite', 34: 'baseball bat', 35: 'baseball glove', 36: 'skateboard', 37: 'surfboard', 38: 'tennis racket', 39: 'bottle', 
+# 40: 'wine glass', 41: 'cup', 42: 'fork', 43: 'knife', 44: 'spoon', 45: 'bowl', 46: 'banana', 47: 'apple', 48: 'sandwich', 
+# 49: 'orange', 50: 'broccoli', 51: 'carrot', 52: 'hot dog', 53: 'pizza', 54: 'donut', 55: 'cake', 56: 'chair', 57: 'couch', 
+# 58: 'potted plant', 59: 'bed', 60: 'dining table', 61: 'toilet', 62: 'tv', 63: 'laptop', 64: 'mouse', 65: 'remote', 
+# 66: 'keyboard', 67: 'cell phone', 68: 'microwave', 69: 'oven', 70: 'toaster', 71: 'sink', 72: 'refrigerator', 73: 'book', 
+# 74: 'clock', 75: 'vase', 76: 'scissors', 77: 'teddy bear', 78: 'hair drier', 79: 'toothbrush'}
